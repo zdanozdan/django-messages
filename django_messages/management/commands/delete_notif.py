74cf35cec7aba0ceba71837f2df2106719aeeb93
zdanozdan <tomasz@enduhub.com>
2022-09-10 21:16:40 +0000

    added delete auto notifications

---
+++ django_messages/management/commands/delete_notif.py
@@ -0,0 +1,15 @@
+import datetime
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+from ...models import Message
+
+
+class Command(BaseCommand):
+    help = (
+        'Deletes all system notif messages'
+    )
+
+    def handle(self, *args, **options):
+        Message.objects.filter(
+            notif_type=Message.RESULTS_NOTIF,
+        ).delete()
